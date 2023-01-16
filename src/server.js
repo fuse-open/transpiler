0432622d631ec146be91d4a22cf10d5541da8400
Morten Daniel Fornes <mdfornes@gmail.com>
2023-01-16 19:57:35 +0700

    format source code
    * Use "const" instead of "var"
    * Remove semicolons

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-var babel = require("@babel/core");
-var babel_preset_env = require("@babel/preset-env");
-var babel_preset_typescript = require("@babel/preset-typescript");
-var babel_plugin_proposal_decorators = require("@babel/plugin-proposal-decorators");
-var babel_plugin_proposal_class_properties = require("@babel/plugin-proposal-class-properties");
-var babel_plugin_proposal_object_rest_spread = require("@babel/plugin-proposal-object-rest-spread");
-var babel_plugin_proposal_optional_chaining = require("@babel/plugin-proposal-optional-chaining");
-var babel_plugin_transform_typescript_metadata = require('babel-plugin-transform-typescript-metadata');
-var http = require("http");
+const babel = require("@babel/core")
+const babel_preset_env = require("@babel/preset-env")
+const babel_preset_typescript = require("@babel/preset-typescript")
+const babel_plugin_proposal_decorators = require("@babel/plugin-proposal-decorators")
+const babel_plugin_proposal_class_properties = require("@babel/plugin-proposal-class-properties")
+const babel_plugin_proposal_object_rest_spread = require("@babel/plugin-proposal-object-rest-spread")
+const babel_plugin_proposal_optional_chaining = require("@babel/plugin-proposal-optional-chaining")
+const babel_plugin_transform_typescript_metadata = require("babel-plugin-transform-typescript-metadata")
+const http = require("http")
 
 function transpile(filename, code) {
-    var ext = filename.split('.').pop().toLowerCase();
-    var plugins = [
+    const ext = filename.split('.').pop().toLowerCase()
+    const plugins = [
         // NB: plugin-transform-typescript-metadata must go before
         //     plugin-proposal-decorators!
         babel_plugin_transform_typescript_metadata,
@@ -23,7 +23,7 @@ function transpile(filename, code) {
         babel_plugin_proposal_class_properties,
         babel_plugin_proposal_object_rest_spread,
         babel_plugin_proposal_optional_chaining
-    ];
+    ]
 
     if (ext == "ts") {
         return babel.transform(code, {
@@ -34,7 +34,7 @@ function transpile(filename, code) {
             ],
             plugins: plugins,
             sourceMaps: "inline"
-        });
+        })
     } else {
         return babel.transform(code, {
             filename: filename,
@@ -43,44 +43,44 @@ function transpile(filename, code) {
             ],
             plugins: plugins,
             sourceMaps: "inline"
-        });
+        })
     }
 }
 
-var server = http.createServer(function(request, response) {
+const server = http.createServer(function(request, response) {
     if (request.method !== "POST") {
-        response.writeHead(404);
-        response.end();
-        return;
+        response.writeHead(404)
+        response.end()
+        return
     }
 
-    var chunks = [];
+    const chunks = []
 
     request.on("data", function(data) {
-        chunks.push(data);
-    });
+        chunks.push(data)
+    })
 
     request.on("end", function() {
         try {
-            var input = JSON.parse(Buffer.concat(chunks));
+            const input = JSON.parse(Buffer.concat(chunks))
 
             if (input.quit) {
-                response.end();
-                process.exit(0);
+                response.end()
+                process.exit(0)
             }
 
             response.end(JSON.stringify({
                 code: transpile(input.filename, input.code).code
-            }));
+            }))
         } catch (err) {
             response.end(JSON.stringify({
                 message: err.message,
                 codeFrame: err.codeFrame
-            }));
+            }))
         }
-    });
+    })
 })
 
 server.listen(0, "127.0.0.1", function() {
-    process.stdout.write("port:" + server.address().port + "\n");
-});
+    process.stdout.write("port:" + server.address().port + "\n")
+})
